f78d69ed43c3f8a8397cc35c75d9067cdcb6e2e3
Jakub Novák <jakubnovak97@gmail.com>
2019-11-26 12:44:04 +0100

    test for FixedOverlapping should always exit with status 1, therefore it fails

--- test/Feature/DefineFixedOverlapping.c
+++ test/Feature/DefineFixedOverlapping.c
@@ -1,15 +1,16 @@
 // RUN: %clang %s -g -emit-llvm %O0opt -c -o %t1.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out --emit-all-errors %t1.bc 2>&1 | FileCheck %s
+// XFAIL: *
+// RUN: %klee --exit-on-error --output-dir=%t.klee-out %t1.bc
+
 #include <stdio.h>
 
 #define ADDRESS ((int*) 0x0080)
 #define ADDRESS2 ((int*) 0x0084)
 
 int main() {
-  klee_define_fixed_object(ADDRESS, 8); 
-	klee_define_fixed_object(ADDRESS2, 4); // CHECK: Trying to allocate an overlapping object 
-  int *p = ADDRESS;	
+	klee_define_fixed_object(ADDRESS, 8);
+	klee_define_fixed_object(ADDRESS2, 4);
+	int *p = ADDRESS;
 	int *r = ADDRESS2; 
-	
 }
